3567065ef3f82751054a9d3571d6d0b2308e817c
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-10-01 00:01:32 +0500

    update 30.09

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -104,6 +104,7 @@ export default defineNuxtConfig({
       ],
     },
     workbox: {
+      navigateFallback: "/",
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
     },
     injectManifest: {
