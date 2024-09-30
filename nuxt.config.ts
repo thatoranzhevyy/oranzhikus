0c268fb722549a49fbb369358b68774536b08247
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-09-30 23:56:54 +0500

    update 30.09

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxtjs/i18n',
     'dayjs-nuxt',
+    '@vite-pwa/nuxt',
   ],
   app: {
     head: {
@@ -32,4 +33,91 @@ export default defineNuxtConfig({
   colorMode,
   i18n,
   dayjs,
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Glitch',
+      short_name: 'Glitch',
+      description: "Testing Glitch PWA",
+      display: 'fullscreen',
+      icons: [
+        {
+          "src": "assets/icons/icon-36x36.png",
+          "sizes": "36x36",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-48x48.png",
+          "sizes": "48x48",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png",
+          "purpose": "maskable any"
+        },
+        {
+          "src": "assets/icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable any"
+        }
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
 })
