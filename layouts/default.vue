83822df62806078440d2b105d27e4f5a6bfefd2e
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-10-01 21:46:46 +0500

    update 01.10

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <VitePwaManifest/>
   <main class="min-h-dvh min-w-screen pb-14 bg-gray-150 dark:bg-gray-950">
     <slot/>
   </main>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,24 +35,59 @@ export default defineNuxtConfig({
   dayjs,
   pwa: {
     manifest: {
-      name: "pwa nuxt 3",
-      short_name: "pwa nuxt",
-      theme_color: '#8a8a5e',
-      description: "Arman Abi r.man.abi@gmail.com",
+      name: 'LearningBy',
+      short_name: 'LearningBy',
+      description: "Testing LearningBy PWA",
+      display: 'fullscreen',
       icons: [
         {
-          src: 'image.png',
-          sizes: "150x150",
-          type: "image/png"
+          "src": "icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
         },
-      ]
+        {
+          "src": "icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
     },
     workbox: {
       navigateFallback: "/",
     },
     devOptions: {
       enabled: true,
-      type: "module"
-    }
-  }
+      type: "module",
+    },
+  },
 })
